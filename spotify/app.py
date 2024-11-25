66f4153061c4cc0c6cfc7e08b354b1f45e6b19da
Lucy Armitage <la724@imperial.ac.uk>
2024-11-25 23:19:19 +0000

    edit app.py to match index.html

--- spotify/app.py
+++ spotify/app.py
@@ -8,6 +8,12 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/query", methods=["POST"])
+def query():
+    spotify_artist = request.form.get("spotify_artist")
+    return f"You entered: {spotify_artist}"
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
