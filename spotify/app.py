602523df96de5762e2ac0b045ea388a20b2ce332
Lucy Armitage <la724@imperial.ac.uk>
2024-11-26 09:38:11 +0000

    changed app.py to match index.html

--- spotify/app.py
+++ spotify/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/query", methods=["POST"])
+def query():
+    spotify_artist = request.form.get("spotify_artist")
+    return f"You entered: {spotify_artist}"
 
 
 if __name__ == "__main__":
