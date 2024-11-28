863237e21345349513d015ee6f5904b0d34b415c
amisha-kunde <amisha.kunde@gmail.com>
2024-11-28 12:13:05 +0000

    changes

--- spotify/app.py
+++ spotify/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify, render_template, redirect, url_for
 import os
-import requests
 from APIQueries import artist_search, get_top_tracks
 
 app = Flask(__name__)
