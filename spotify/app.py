4450a2ed31604f9868cec0a592c702c496eefd4f
Lucy Armitage <la724@imperial.ac.uk>
2024-12-08 10:50:42 +0000

    env

--- spotify/app.py
+++ spotify/app.py
@@ -8,6 +8,7 @@ from flask import (
 )
 import os
 import random
+from dotenv import load_dotenv
 import sqlite3
 from spotify.APIQueries import (
     get_token,
@@ -25,6 +26,9 @@ from spotify.databases import (
 
 # Initialize Flask app
 app = Flask(__name__)
+
+# Load environment variables
+load_dotenv()
 
 # Configure Flask secret key
 app.secret_key = os.urandom(24)
